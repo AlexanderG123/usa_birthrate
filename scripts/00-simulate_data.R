e0731a3e90889a3f8aa9469290edba588322406a
Alexander Guarasci <alexander.guarasci@mail.utoronto.ca>
2024-02-02 15:21:07 -0500

    Simulate and test data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -13,7 +13,49 @@ library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(8)
+years <- 1980:2020
+
+#simulated data for fig_1
+mean_brate <- 65
+sd_brate <- 3
+brate_all_simulated <- rnorm(length(years), mean_brate, sd_brate)
+simulated_data <- data.frame(year = years, brate_all = brate_all_simulated)
+write_csv(x = simulated_data, file = "outputs/data/sim_fig_1.csv")
+
+
+
+
+#simulate data for fig_4
+
+brate_1519 <- rnorm(length(years), mean = 50, sd = 10)
+brate_2024 <- rnorm(length(years), mean = 120, sd = 10)
+brate_2529 <- rnorm(length(years), mean = 110, sd = 10)
+brate_3034 <- rnorm(length(years), mean = 60, sd = 10)
+brate_3539 <- rnorm(length(years), mean = 20, sd = 10)
+brate_4044 <- rnorm(length(years), mean = 5, sd = 10)
+simulated_data2 <- data.frame(year = years, brate_1519 = brate_1519, brate_2024 = brate_2024,
+                              brate_2529 = brate_2529, brate_3034 = brate_3034, brate_3539 = brate_3539,
+                              brate_4044 = brate_4044)
+simulated_data2
+write_csv(x = simulated_data2, file = "outputs/data/sim_fig_4.csv")
+
+# simulate data for fig_2e
+brate_unmarried <- rnorm(length(years), mean = 40, sd = 5)
+brate_married <- rnorm(length(years), mean = 90, sd = 5)
+simulated_data3 <- data.frame(year = years, brate_unmarried = brate_unmarried, brate_married = brate_married)
+write_csv(x = simulated_data3, file = "outputs/data/sim_fig_2e.csv")
+
+#simulate data for fig_5
+brate_lesshs <- rnorm(length(years), mean = 100, sd = 10)
+brate_hsgrad <- rnorm(length(years), mean = 75, sd = 5)
+brate_somecoll <- rnorm(length(years), mean = 60, sd = 5)
+brate_colgrad <- rnorm(length(years), mean = 70, sd = 5)
+
+simulated_data4 <- data.frame(year = years, brate_lesshs = brate_lesshs, brate_hsgrad = brate_hsgrad,
+                             brate_somecoll = brate_somecoll, brate_colgrad = brate_colgrad)
+
+write_csv(x = simulated_data4, file = "outputs/data/sim_fig_5.csv")
 
 
 
--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -10,6 +10,50 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
-#### Test data ####
+#test fig_1 data
+data1 = read_csv("~/usa_birthrate/inputs/data/fig_1.csv")
+#test all data is inbetween 1980 and 2020
+all(data1$year >= 1980 & data$occurrence_year <= 2020)
+
+# test brate_all is numeric
+all(sapply(data1$brate_all, is.numeric))
+
+#test fig_2e data 
+data2 = read_csv("~/usa_birthrate/inputs/data/fig_2e.csv")
+#test all data is inbetween 1980 and 2020
+all(data2$year >= 1980 & data$occurrence_year <= 2020)
+
+# test brate is numeric
+all(sapply(data2$brate_unmarried, is.numeric))
+all(sapply(data2$brate_married, is.numeric))
+
+#test fig_4 data
+data3 = read_csv("~/usa_birthrate/inputs/data/fig_4.csv")
+
+#test all data is inbetween 1980 and 2020
+all(data3$year >= 1980 & data$occurrence_year <= 2020)
+
+# test brate is numeric
+all(sapply(data3$brate_1519, is.numeric))
+all(sapply(data3$brate_2024, is.numeric))
+all(sapply(data3$brate_2529, is.numeric))
+all(sapply(data3$brate_3034, is.numeric))
+all(sapply(data3$brate_3539, is.numeric))
+all(sapply(data3$brate_4044, is.numeric))
+
+
+#test fig_5 data
+data4 = read_csv("~/usa_birthrate/inputs/data/fig_5.csv")
+
+#test all data is inbetween 1980 and 2020
+all(data4$year >= 1980 & data$occurrence_year <= 2020)
+
+# test brate is numeric
+all(sapply(data4$brate_lesshs, is.numeric))
+all(sapply(data4$brate_hsgrad, is.numeric))
+all(sapply(data4$brate_somecoll, is.numeric))
+all(sapply(data4$brate_colgrad, is.numeric))
+
+
+
